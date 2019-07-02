d091c43a2d49dacd0d6d1e22dba3423c3bd8a3a0
xrotwang <xrotwang@googlemail.com>
2019-07-02 17:50:07 +0200

    fixed tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -10,7 +10,7 @@ def test_Dataset(mocker):
     refs = list(ds.iterreferences())
     assert len(refs) == 106
     dps = list(ds.iterdata())
-    assert len(dps) == 3834
+    assert len(dps) == 3827
     assert len(list(ds.itercontributions())) == 30
     assert len(list(ds.itermethods())) == 1032
-    assert ds.validate(log=mocker.Mock()) == 298
+    assert ds.validate(log=mocker.Mock()) == 46
