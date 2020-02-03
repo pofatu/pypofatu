aa21d086ad4b982b828bdade6ffec2dcd6acf71d
xrotwang <xrotwang@googlemail.com>
2020-02-03 13:32:59 +0100

    try to fix travis

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'dev': ['flake8', 'wheel', 'twine'],
         'test': [
             'mock',
-            'pytest>=3.6',
+            'pytest>=4.3',
             'pytest-mock',
             'pytest-cov',
             'coverage>=4.2',
